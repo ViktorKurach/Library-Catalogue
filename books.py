33a30bf288421b2f3b382a52a07fab3810eba6db
ViktorKurach <mozepenko@gmail.com>
2017-03-30 23:29:51 +0300

    Filter functions in copy-paste style are rewritten

--- books.py
+++ books.py
@@ -1,5 +1,5 @@
 def add_book(catalogue, new):
-    """Adds new book to the catalogue. Returns 1 if the book is already in catalogue, or 0 otherwise. 'new' parameter \
+    """Adds new book to the catalogue. Returns 1 if the book is already in catalogue, or 0 otherwise. Parameter 'new' \
     must be a dictionary with keys: 'path', 'title', 'author', 'publication', 'genre', 'description', 'year'"""
     if new in catalogue:
         return 1
@@ -33,7 +33,7 @@ def delete_book(catalogue, title, author):
         return 1
 
 
-def sort_by_year(book_list, desc=0):
+def sort_by_year(book_list, desc):
     """Returns book list sorted by year. If desc=1, sorts by increase; if desc=-1, sorts by decrease; otherwise \
     sorting is not performed"""
     if desc == 1:
@@ -43,47 +43,29 @@ def sort_by_year(book_list, desc=0):
     return book_list
 
 
-def filter_by_year(catalogue, year_from, year_to, desc=0):
-    """Returns books, published between 'year_from' and 'year_to' and sorted according to 'desc' parameter"""
-    res = []
-    for x in catalogue:
-        if (x["year"] in range(year_from, year_to+1)) and (x not in res):
-            res.append(x)
+def filter_books(catalogue, key, value, year_from=None, year_to=None, desc=0):
+    """Filters books by the key and/or publication year. Key must be "author", "genre", or "year". Value is author's \
+    or genre's name; if key="year", any value is allowed. If year_from=None, filtering by year is not performed. If \
+    year_to=None, returns only books published in year_from. If desc=1, books are sorted by publish date increasing; \
+    if desc=-1, are sorted by date decreasing; otherwise sorting is not performed"""
+    if key not in ["author", "genre", "year"]:
+        return
+    buf, res = [], []
+    if key == "year":
+        buf = catalogue
+    else:
+        for x in catalogue:
+            if (x[key] == value) and (x not in buf):
+                buf.append(x)
+    if year_to is None:
+        year_to = year_from
+    if year_from is not None:
+        for x in buf:
+            if (x["year"] in range(year_from, year_to+1)) and (x not in res):
+                res.append(x)
+    else:
+        res = buf
     return sort_by_year(res, desc)
-
-
-def filter_by_author(catalogue, author, year_from=None, year_to=None, desc=0):
-    """Returns books, published by one author between 'year_from' and 'year_to' and sorted by year according to 'desc' \
-    parameter. If either year_from=None or year_to=None, returns books published in only one year; if both \
-    year_from=None and year_to=None, filtering by year is not performed"""
-    res = []
-    for x in catalogue:
-        if (x["author"] == author) and (x not in res):
-            res.append(x)
-    if year_from and year_to:
-        return filter_by_year(res, year_from, year_to, desc)
-    elif not year_to:
-        return filter_by_year(res, year_from, year_from, desc)
-    elif not year_from:
-        return filter_by_year(res, year_to, year_to, desc)
-    return res
-
-
-def filter_by_genre(catalogue, genre, year_from=None, year_to=None, desc=0):
-    """Returns books of one genre, published between 'year_from' and 'year_to' and sorted by year according to 'desc' \
-    parameter. If either year_from=None or year_to=None, returns books published in only one year; if both \
-    year_from=None and year_to=None, filtering by year is not performed"""
-    res = []
-    for x in catalogue:
-        if (x["genre"] == genre) and (x not in res):
-            res.append(x)
-    if year_from and year_to:
-        return filter_by_year(res, year_from, year_to, desc)
-    elif not year_to:
-        return filter_by_year(res, year_from, year_from, desc)
-    elif not year_from:
-        return filter_by_year(res, year_to, year_to, desc)
-    return res
 
 
 def get_authors(catalogue, genre=None):
@@ -117,7 +99,7 @@ add_book(books, book1)
 print(books)
 print(add_book(books, book1))
 print(add_book(books, book4))
-print(search_book(books, "Бійцівський клуб", "Чак Поланик"))
+print(search_book(books, "Бійцівський клуб", "Чак Поланік"))
 print(search_book(books, "Колекціонер", "Джон Фаулз"))
 new_book = {"path": "C:\\Books", "title": "Перехресні стежки", "author": "Іван Франко", "publication": "Фоліо",
             "genre": "Повість", "description": "Класика укр. літ.", "year": 1900}
@@ -125,8 +107,15 @@ edit_book(books, "Бійцівський клуб", "Чак Поланик", new
 edit_book(books, "Перехресні стежки", "Іван Франко", new_book)
 print(delete_book(books, "Колекціонер", "Джон Фаулз"))
 print(delete_book(books, "Бійцівський клуб", "Чак Поланік"))
-print(filter_by_author(books, "Сергій Жадан", 2009, 2012))
-print(filter_by_genre(books, "Повість", 1900))
+print(filter_books(books, "year", "anything", None, None, -1))
+print(filter_books(books, "year", None, 2010))
+print(filter_books(books, "year", 228, 2005, 2017))
+print(filter_books(books, "author", "Сергій Жадан", None, None, -1))
+print(filter_books(books, "author", "Іван Франко", 1900))
+print(filter_books(books, "author", "Сергій Жадан", 2007, 2012))
+print(filter_books(books, "genre", "Повість", None, 2114, 1))
+print(filter_books(books, "genre", "Повість", 1900))
+print(filter_books(books, "genre", "Поезія", 1800, 2050, 1))
 print(get_authors(books))
 print(get_authors(books, "Роман"))
 print(get_authors(books, "Детектив"))
