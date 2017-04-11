2f0f1e77458e8c83c6aa4ade83132dbfcd35058a
ViktorKurach <mozepenko@gmail.com>
2017-04-11 20:26:44 +0300

    books.py edited according to pep8

--- books.py
+++ books.py
@@ -1,6 +1,8 @@
 def add_book(catalogue, new):
-    """Adds new book to the catalogue. Returns 1 if the book is already in catalogue, or 0 otherwise. Parameter 'new' \
-    must be a dictionary with keys: 'path', 'title', 'author', 'publication', 'genre', 'description', 'year'"""
+    """Adds new book to the catalogue. Returns 1 if the book is already in \
+    catalogue, or 0 otherwise. Parameter 'new' must be a dictionary with \
+    keys: 'path', 'title', 'author', 'publication', 'genre', 'description', \
+    'year'"""
     if new in catalogue:
         return 1
     catalogue.append(new)
@@ -8,14 +10,16 @@ def add_book(catalogue, new):
 
 
 def search_book(catalogue, title, author):
-    """Returns a dictionary corresponding to a book with 'title' written by 'author'. If not found - returns None"""
+    """Returns a dictionary corresponding to a book with 'title' written by \
+    'author'. If not found - returns None"""
     for x in catalogue:
         if (x["title"] == title) and (x["author"] == author):
             return x
 
 
 def edit_book(catalogue, old_title, old_author, new):
-    """Replaces a book with a new one. Returns new book in case of success, or None if old book is not found"""
+    """Replaces a book with a new one. Returns new book in case of success, \
+    or None if old book is not found"""
     book = search_book(catalogue, old_title, old_author)
     if book:
         i = catalogue.index(book)
@@ -25,7 +29,8 @@ def edit_book(catalogue, old_title, old_author, new):
 
 
 def delete_book(catalogue, title, author):
-    """Removes a book from a catalogue. Returns 0 in case of success, or 1 if the book is not found"""
+    """Removes a book from a catalogue. Returns 0 in case of success, or 1 \
+    if the book is not found"""
     try:
         catalogue.remove(search_book(catalogue, title, author))
         return 0
@@ -34,8 +39,10 @@ def delete_book(catalogue, title, author):
 
 
 def sort_by_year(book_list, desc):
-    """Returns book list sorted by year according to 'desc' parameter (see filter_books description).
-    In next versions function will be private: usage outside this module is not recommended"""
+    """Returns book list sorted by year according to 'desc' parameter (see \
+    filter_books description).
+    In next versions function will be private: usage outside this module is \
+    not recommended"""
     if desc == 1:
         return sorted(book_list, key=lambda x: x["year"])
     if desc == -1:
@@ -44,9 +51,11 @@ def sort_by_year(book_list, desc):
 
 
 def filter_by_year(book_list, year_from, year_to, desc):
-    """Filters books from book list, published between 'year_from' and 'year_to' and sorted according to 'desc' \
-    parameter (see filter_books description).
-    In next versions function will be private: usage outside this module is not recommended"""
+    """Filters books from book list, published between 'year_from' and \
+    'year_to' and sorted according to 'desc' parameter (see filter_books \
+    description).
+    In next versions function will be private: usage outside this module is \
+    not recommended"""
     if year_from is None:
         return sort_by_year(book_list, desc)
     if year_to is None:
@@ -59,10 +68,13 @@ def filter_by_year(book_list, year_from, year_to, desc):
 
 
 def filter_books(catalogue, key, value, year_from=None, year_to=None, desc=0):
-    """Filters books by the key and/or publication year. Key must be "author", "genre", or "year". Value is author's \
-    or genre's name; if key="year", any value is allowed. If year_from=None, filtering by year is not performed. If \
-    year_to=None and not year_from=None, returns only books published in year_from. If desc=1, books are sorted by \
-    publish year increasing; if desc=-1, are sorted by year decreasing; otherwise sorting is not performed"""
+    """Filters books by the key and/or publication year. Key must be "author",\
+     "genre", or "year". Value is author's or genre's name; if key="year", \
+     any value is allowed. If year_from=None, filtering by year is not \
+     performed. If year_to=None and not year_from=None, returns only books \
+     published in year_from. If desc=1, books are sorted by publish year \
+     increasing; if desc=-1, are sorted by year decreasing; otherwise sorting \
+     is not performed"""
     if key not in ["author", "genre", "year"]:
         return
     if key == "year":
@@ -75,7 +87,8 @@ def filter_books(catalogue, key, value, year_from=None, year_to=None, desc=0):
 
 
 def get_authors(catalogue, genre=None):
-    """Returns a list of authors, whose books relate to genre. If genre=None, returns all authors in the catalogue"""
+    """Returns a list of authors, whose books relate to genre. If genre=None, \
+    returns all authors in the catalogue"""
     authors = []
     for x in catalogue:
         if (x["genre"] == genre) and (x["author"] not in authors):
