6be3cbb41665d2c716616a3131c85e2296445f87
ViktorKurach <mozepenko@gmail.com>
2017-05-10 14:56:04 +0300

    Documentation for books.py standartized

--- books.py
+++ books.py
@@ -1,8 +1,10 @@
 def add_book(catalogue, new):
     """
-    Adds new book to the catalogue. Returns 1 if the book is already in
-    catalogue, or 0 otherwise. Parameter 'new' must be a dictionary with keys:
-    'path', 'title', 'author', 'publication', 'genre', 'description', 'year'.
+    Adds new book to the catalogue.
+    :param catalogue: a list of books, which new book is added to.
+    :param new: a dictionary with next keys: 'path', 'title', 'author',
+    'publication', 'genre', 'description', 'year'.
+    :returns 1 if the book is already in catalogue, or 0 otherwise.
 
     >>> catalogue = []
     >>> book = {"path": "Path", "title": "T", "author": "A",\
@@ -20,8 +22,12 @@ def add_book(catalogue, new):
 
 def search_book(catalogue, title, author):
     """
-    Returns a dictionary corresponding to a book. If not found - returns
-    nothing.
+    Searches a book in the catalogue.
+    :param catalogue: a list of books, which the search is performed in.
+    :param title: a string - title of the book, which is searched.
+    :param author: a string - name of author of the book, which is searched.
+    :returns a dictionary, corresponding to searched book, or nothing, if book
+    is not found.
 
     >>> book = {"path": "Path", "title": "T", "author": "A",\
     "publication": "P", "genre": "G", "description": "D", "year": 2017}
@@ -38,8 +44,15 @@ def search_book(catalogue, title, author):
 
 def edit_book(catalogue, old_title, old_author, new):
     """
-    Replaces a book with a new one. Returns new book in case of success, or
-    nothing if old book is not found.
+    Edits a book in the catalogue.
+    :param catalogue: a list of books.
+    :param old_title: a string - title of the book, which will be edited.
+    :param old_author: a string - author of the book, which will be edited.
+    :param new: a dictionary, containing new information about the book;
+    must have keys: 'path', 'title', 'author', 'publication', 'genre',
+    'description', 'year'.
+    :returns new book in case of success, or nothing, if old book is not
+    found in the catalogue.
 
     >>> book = {"path": "Path", "title": "TT", "author": "AA",\
     "publication": "P", "genre": "G", "description": "D", "year": 1800}
@@ -61,8 +74,11 @@ def edit_book(catalogue, old_title, old_author, new):
 
 def delete_book(catalogue, title, author):
     """
-    Removes a book from a catalogue. Returns 0 in case of success, or 1 if the
-    book is not found.
+    Removes a book from the catalogue.
+    :param catalogue: a list of books.
+    :param title: a string - title of the book, which will be deleted.
+    :param author: a string - author of the book, which will be deleted.
+    :returns 0 in case of success, or 1, if the book is not found in catalogue.
 
     >>> book = {"path": "Path", "title": "T", "author": "A",\
     "publication": "P", "genre": "G", "description": "D", "year": 2017}
@@ -81,13 +97,16 @@ def delete_book(catalogue, title, author):
 
 def sort_by_year(book_list, desc):
     """
-    Returns book list sorted by year according to 'desc' parameter (see
-    filter_books description).
+    Sorts a list of books by publishing year.
+    :param book_list: a list of books, that is sorted.
+    :param desc: if 1, sorts books by publishing year increasing, if -1 - by
+    publishing year decreasing; otherwise sorting is not performed.
+    :returns a sorted list of books.
 
     In next versions function will be private: usage outside this module is
     not recommended.
 
-    Function is always called by filter_by_year function: see tests there.
+    Function is always called by filter_by_year(): see tests there.
     """
     if desc == 1:
         return sorted(book_list, key=lambda x: x["year"])
@@ -98,13 +117,21 @@ def sort_by_year(book_list, desc):
 
 def filter_by_year(book_list, year_from, year_to, desc):
     """
-    Filters books from book list, published between year_from and year_to and
-    sorted according to 'desc' parameter (see filter_books description).
+    Filters books by publishing year and/or sorts them.
+    :param book_list: a list of books, that is filtered.
+    :param year_from: an integer, lower limit of publishing years' diapason;
+    if year_from=None, filtering is not performed.
+    :param year_to: an integer, higher limit of publishing year's diapason;
+    if year_to=None and year_from is not None, searches only books, published
+    in year_from; if year_from=None, any year_to is allowed.
+    :param desc: if 1, sorts selected books by publishing year increasing,
+    if -1 - by publishing year decreasing; otherwise sorting is not performed.
+    :returns a filtered and sorted list of books.
 
     In next versions function will be private: usage outside this module is
     not recommended.
 
-    Function is always called by filter_books function: see tests there.
+    Function is always called by filter_books(): see tests there.
     """
     if year_from is None:
         return sort_by_year(book_list, desc)
@@ -119,15 +146,19 @@ def filter_by_year(book_list, year_from, year_to, desc):
 
 def filter_books(catalogue, key, value, year_from=None, year_to=None, desc=0):
     """
-    Filters books by the key and/or publication year. Key must be "author",
-    "genre", or "year". Value is author's or genre's name; if key="year", any
-    value is allowed.
-
-    If year_from=None, filtering by year is not performed. If year_to=None and
-    not year_from=None, returns only books published in year_from.
-
-    If desc=1, books are sorted by publish year increasing; if desc=-1, are
-    sorted by year decreasing; otherwise sorting is not performed.
+    Filters books by the key and/or publishing year and sorts them.
+    :param catalogue: a list of books, that is filtered.
+    :param key: "genre", "author", or "year".
+    :param value: a string, genre's name, if key="genre"; a string, author's
+    name, if key="author"; if key="year", any value is allowed.
+    :param year_from: an integer, lower limit of publishing years' diapason;
+    if year_from=None, filtering by year is not performed.
+    :param year_to: an integer, higher limit of publishing year's diapason;
+    if year_to=None and year_from is not None, searches only books, published
+    in year_from; if year_from=None, any year_to is allowed.
+    :param desc: if 1, sorts selected books by publishing year increasing,
+    if -1 - by publishing year decreasing; otherwise sorting is not performed.
+    :returns a filtered and sorted list of books.
 
     >>> book1 = {"path": "Path", "title": "T1", "author": "A1",\
     "publication": "P", "genre": "G", "description": "D", "year": 1900}
@@ -218,8 +249,11 @@ def filter_books(catalogue, key, value, year_from=None, year_to=None, desc=0):
 
 def get_authors(catalogue, genre=None):
     """
-    Returns a list of authors, whose books relate to genre. If genre=None,
-    returns all authors in the catalogue.
+    Selects authors, whose books relate to genre.
+    :param catalogue: a list of books.
+    :param genre: a string - name of the genre, authors must relate to;
+    if None, returns all of the authors in catalogue.
+    :returns list of strings - authors' names.
 
     >>> book1 = {"path": "Path", "title": "T1", "author": "A1",\
     "publication": "P", "genre": "G1", "description": "D", "year": 1900}
